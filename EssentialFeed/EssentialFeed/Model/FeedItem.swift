d9227b04e9b7c88761de333ad8e87c4e1f5e32f5
Humberto Castro <hcstr@icloud.com>
2022-12-27 21:24:01 -0300

    feed item

---
+++ EssentialFeed/EssentialFeed/Model/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Humberto Castro on 27/12/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
